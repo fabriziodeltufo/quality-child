cd0636158fab97a61c382f93eb5e9724de462bad
FABRIZIO DEL TUFO <neo@WAIKIKI.local>
2024-02-25 11:42:58 +0200

    quality child style.css header upd

--- functions.php
+++ functions.php
@@ -4,14 +4,17 @@
 // STYLES 
 function my_theme_enqueue_styles() {
 
-       $parent_style = 'quality-style'; // parent theme
 
-       wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+       // PARENT THEME : get_template_directory_uri() -- quality theme
+       wp_enqueue_style('quality-style', get_template_directory_uri() . '/style.css');
 
-       wp_enqueue_style( 'quality-child-style', get_stylesheet_directory_uri() . '/style.css',
-       array( $parent_style ),
-       wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
-	);
+
+       // CHILD THEME : get_stylesheet_directory_uri() -- quality child theme
+       wp_enqueue_style( 'quality-child-style', get_stylesheet_directory_uri() . '/style.css', 
+       array( 'quality-style' ) , wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
+       );
+
+
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
